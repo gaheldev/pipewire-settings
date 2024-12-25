acbb7b0372d8092afef3f50b955899c08098eb1d
Gahel <music@gahel.fr>
2024-12-25 16:25:04 +0100

    Increase debugging window size

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 ./deploy.sh
+# export G_MESSAGES_DEBUG=all
+export MUTTER_DEBUG_DUMMY_MODE_SPECS=2048x1240
+export SHELL_DEBUG=backtrace-segfaults
 dbus-run-session -- gnome-shell --nested --wayland
