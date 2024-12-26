85a5d1c8e99187c48aa645d3154017be8dd9ad55
Gahel <music@gahel.fr>
2024-12-26 05:52:04 +0100

    chore: indentation

--- pipewire-settings@gaheldev.github.com/extension.js
+++ pipewire-settings@gaheldev.github.com/extension.js
@@ -214,19 +214,19 @@ class Indicator extends PanelMenu.Button {
 
 // Function to run a command synchronously and wait for it to finish
 function runCommand(command, args) {
-        let subprocess = new Gio.Subprocess({
-            argv: [command, ...args],
-            flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
-        });
+    let subprocess = new Gio.Subprocess({
+        argv: [command, ...args],
+        flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+    });
 
-        subprocess.init(null);
+    subprocess.init(null);
 
-        let [ok, stdout, stderr] = subprocess.communicate_utf8(null, null);
-        if (ok) {
-            return stdout;
-        } else {
-            throw (new Error(stderr));
-        }
+    let [ok, stdout, stderr] = subprocess.communicate_utf8(null, null);
+    if (ok) {
+        return stdout;
+    } else {
+        throw (new Error(stderr));
+    }
 }
 
 
