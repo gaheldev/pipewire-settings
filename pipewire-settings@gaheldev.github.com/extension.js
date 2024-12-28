e5c9e3924bfc255b320002bc6f758955092a65dc
Gahel <music@gahel.fr>
2024-12-28 01:32:54 +0100

    Remove dead code

--- pipewire-settings@gaheldev.github.com/extension.js
+++ pipewire-settings@gaheldev.github.com/extension.js
@@ -21,15 +21,13 @@ import St from 'gi://St';
 import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 
-
-/* Gio.Subprocess */
-Gio._promisify(Gio.Subprocess.prototype, "communicate_utf8_async");
-
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
@@ -238,6 +236,7 @@ class Indicator extends PanelMenu.Button {
 });
 
 
+
 // Function to run a command synchronously and wait for it to finish
 function runCommand(command, args) {
     let subprocess = new Gio.Subprocess({
@@ -254,6 +253,7 @@ function runCommand(command, args) {
         throw (new Error(stderr));
     }
 }
+
 
 
 export default class IndicatorExampleExtension extends Extension {
